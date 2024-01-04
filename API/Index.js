b1ed9c088e4d87bfe02e5e11f0c56cf9e9cb0aa4
Cao Hoàng Khang <tinlun0386@gmail.com>
2024-01-04 11:25:09 +0700

    add cors index.js

--- API/Index.js
+++ API/Index.js
@@ -21,7 +21,30 @@ const app = express();
 
 dotenv.config();
 
-app.use(cors());
+var whitelist = ['http://localhost:8000', 'http://localhost:3000']; //white list consumers
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(null, false);
+    }
+  },
+  methods: ['GET', 'PUT', 'POST', 'DELETE', 'OPTIONS'],
+  optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
+  credentials: true, //Credentials are cookies, authorization headers or TLS client certificates.
+  allowedHeaders: [
+    'Content-Type',
+    'Authorization',
+    'X-Requested-With',
+    'device-remember-token',
+    'Access-Control-Allow-Origin',
+    'Origin',
+    'Accept'
+  ]
+};
+
+app.use(cors(corsOptions));
 
 // parse json request body
 app.use(express.json());
