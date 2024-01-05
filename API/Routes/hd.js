90daf01fd83bdb6ed119dd17b6b4286a321f62c1
dhnbroken <bdfnatsu@gmail.com>
2024-01-05 09:07:29 +0700

    fix: change url endpoint bill

--- API/Routes/hd.js
+++ API/Routes/hd.js
@@ -1,22 +1,22 @@
-import  express  from "express";
-import { VerifyAdmin } from "../Utils/Verifytoken.js";
-import { createHD, createHDCT, getHDCT, getallHD, gethoadon, updateHD, updateHDCT } from "../Controllers/hoadon.js";
+import express from 'express';
+import { VerifyAdmin } from '../Utils/Verifytoken.js';
+import { createHD, createHDCT, getHDCT, getallHD, gethoadon, updateHD, updateHDCT } from '../Controllers/hoadon.js';
 
 const router = express.Router();
 
 //Create HD
-router.post("/", createHD);
+router.post('/', createHD);
 //Create HDCT
-router.post("/:hdId", createHDCT);
+router.post('/:hdId', createHDCT);
 //Update
-router.put("/:id", updateHD);
-router.put("/:id", updateHDCT);
+router.put('/:id', updateHD);
+router.put('/:id', updateHDCT);
 //Delete
 // router.delete("/:id", VerifyAdmin);
 //Get
-router.get("/:id", getHDCT);
+router.get('/student', getHDCT);
 //Getall
-router.get("/", getallHD);
-router.get("/hoadondetails/:id", gethoadon);
+router.get('/', getallHD);
+router.get('/hoadondetails/:id', gethoadon);
 
-export default router
+export default router;
